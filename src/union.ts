679a6326f4b01348bc7e9e7b0e3852357e742992
jate-koh <jate_ben@hotmail.com>
2024-08-03 15:13:11 +0700

    refactor: update union position

--- src/union.ts
+++ src/union.ts
@@ -1,67 +1,68 @@
 import { admin, member, organization } from '@/functions/github';
 
-organization('cpe-kmutt-student');
+organization('kmutt-cpe-student');
 
-admin('riflowth');
+admin('ChaiyapatOam');
+admin('jate-koh')
 
-member('eltfshr');
-member('sokungz01');
-member('Porping');
-member('ChaiyapatOam');
-member('Aquintise');
-member('khemmaphat');
-member('Qandle');
-member('nut1414');
-member('XiaoXuxxxx');
-member('Bukharney');
-member('wit03');
-member('jate-koh');
-member('Thanaphol47825');
-member('DechawatRoyda');
-member('KongJen');
-member('rootEnginear');
-member('BoMoMoBo');
-member('SSSBoOm');
-member('Encall');
-member('KittpongT');
-member('Th1nkK1D');
-member('chogerlate');
-member('PPHamster');
-member('Keetawin');
-member('nnarct');
-member('nitpum');
-member('Arthit3108');
-member('wawawind');
-member('Dr804t');
-member('WTFPUn')
-member('ball0803')
-member('nontaxim');
-member('BlackBoxBanner')
-member('Tanxapol');
-member('bosspod');
-member('wasdee');
-member('Mirailisc');
-member('Billy19191');
-member('ibzzsfw');
-member('parunchxi');
-member('mmawin99');
-member('sin22766');
-member('RONLUG');
-member('EarthWittawat');
-member('OnlyJust3rd')
-member('ChotanansubSoph')
-member('tikpoptv')
-member('PunMung-66')
-member('SuperSonicBoomEX')
-member('njprem')
-member('PEEMAI2004')
-member('NinePTH')
-member('smolguyonearth')
-member('beambeambeam')
-member('SupeemAFK')
-member('Pongpon00')
-member('SinghLemonH')
-member('LuckyThanet')
-member('atiwit')
-member('MiikyMan')
-member('sirawitGOGO')
+member('riflowth');
+member('SorrawitUdom')
+// member('eltfshr');
+// member('Porping');
+// member('sokungz01');
+// member('Aquintise');
+// member('khemmaphat');
+// member('Qandle');
+// member('nut1414');
+// member('XiaoXuxxxx');
+// member('Bukharney');
+// member('wit03');
+// member('Thanaphol47825');
+// member('DechawatRoyda');
+// member('KongJen');
+// member('rootEnginear');
+// member('BoMoMoBo');
+// member('SSSBoOm');
+// member('Encall');
+// member('KittpongT');
+// member('Th1nkK1D');
+// member('chogerlate');
+// member('PPHamster');
+// member('Keetawin');
+// member('nnarct');
+// member('nitpum');
+// member('Arthit3108');
+// member('wawawind');
+// member('Dr804t');
+// member('WTFPUn')
+// member('ball0803')
+// member('nontaxim');
+// member('BlackBoxBanner')
+// member('Tanxapol');
+// member('bosspod');
+// member('wasdee');
+// member('Mirailisc');
+// member('Billy19191');
+// member('ibzzsfw');
+// member('parunchxi');
+// member('mmawin99');
+// member('sin22766');
+// member('RONLUG');
+// member('EarthWittawat');
+// member('OnlyJust3rd')
+// member('ChotanansubSoph')
+// member('tikpoptv')
+// member('PunMung-66')
+// member('SuperSonicBoomEX')
+// member('njprem')
+// member('PEEMAI2004')
+// member('NinePTH')
+// member('smolguyonearth')
+// member('beambeambeam')
+// member('SupeemAFK')
+// member('Pongpon00')
+// member('SinghLemonH')
+// member('LuckyThanet')
+// member('atiwit')
+// member('MiikyMan')
+// member('sirawitGOGO')
